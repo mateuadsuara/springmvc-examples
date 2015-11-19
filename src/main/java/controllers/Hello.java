b9756646abfda29589598075a5a685033dfcc15c
Mateu Adsuara <mateuadsuara@gmail.com>
2015-11-19 19:50:58 +0000

    Adds form option example

--- src/main/java/controllers/Hello.java
+++ src/main/java/controllers/Hello.java
@@ -4,14 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @Controller
 public class Hello {
     @RequestMapping(value = "/*")
     public ModelAndView hello() {
-        System.out.println("HelloController: Passing through...");
-
         ModelAndView mav = new ModelAndView("hello");
-        mav.addObject("msg", "Running hello() method");
+        mav.addObject("countries", getCountries());
+
         return mav;
+    }
+
+    private Map<String, String> getCountries() {
+        Map<String,String> countries = new LinkedHashMap<String,String>();
+        countries.put("US", "United Stated");
+        countries.put("CHINA", "China");
+        countries.put("SG", "Singapore");
+        countries.put("MY", "Malaysia");
+        return countries;
     }
 }
--- web/WEB-INF/views/hello.jsp
+++ web/WEB-INF/views/hello.jsp
@@ -1,7 +1,13 @@
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <html>
 <body>
 <h2>Hello World!</h2>
-
-<h3>${msg}</h3>
+<form>
+    <select>
+        <c:forEach var="country" items="${countries}">
+            <option value="${country.getKey()}">${country.getValue()}</option>
+        </c:forEach>
+    </select>
+</form>
 </body>
 </html>
